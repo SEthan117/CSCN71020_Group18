0cce7637de2d6667bd03602e2a9112022ad7f9ec
Ethan Coates <Ecoates1129@conestogac.on.ca>
2021-11-08 13:07:41 -0500

    Started angle code, removed empty test.

---
+++ PolygonChecker/triangleAngle.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include "triangleAngle.h"
+
+int* triangleAngle(int side1, int side2, int side3)
+{
+	int* angle1;
+	int* angle2;
+	int* angle3;
+	float pi;
+	pi = acos(-1);
+
+	while ((side1 <= 0 || side2 <= 0 || side3 <= 0))
+	{
+
+	}
+}
