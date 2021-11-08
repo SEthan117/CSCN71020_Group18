03d388c4283706174601213bcd93762a66f1e756
Ilyas Yusuf <Iyusuf3055@conestogac.on.ca>
2021-11-08 13:25:53 -0500

    Linked all the unit tests.

---
+++ Group_Project_Unit_Tests/Group_Project_Unit_Tests.cpp
@@ -0,0 +1,25 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+extern "C" {
+
+#include "main.h"
+#include "polygonSolver.h"
+#include "triangleAngle.h"
+#include "triangleSolver.h"
+}
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GroupProjectUnitTests
+{
+	TEST_CLASS(GroupProjectUnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			
+		}
+	};
+}
